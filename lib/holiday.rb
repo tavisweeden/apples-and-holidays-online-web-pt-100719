b7c4cbbdb67c37a6e392c3f81de264375c9035ee
Tavis Weeden <tavisweeden@gmail.com>
2019-11-03 02:53:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,30 +39,32 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 holiday_hash
 end
 
-#require 'pry'
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash.map do |season, holiday_name|
-    season[holiday_name] = supply_array
-
-end
+  holiday_hash[season][holiday_name] = supply_array
 #binding.pry
-holiday_hash
 end
 
+#require 'pry'
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+ holiday_hash[:winter].collect.each do |holiday, supply|
+   supply
+   
+#binding.pry
+end.flatten
 end
 
+#require 'pry'
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
-
+  holiday_hash.each do |season, holiday|
+puts season.to_s.capitalize + ":"
+ holiday.each do |holiday, decorations|
+  h = holiday.to_s.split("_").map{|word|word.capitalize}.join(" ") + ": "
+  s = decorations.join(", ")
+  puts "  " h + s
+  
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
@@ -70,8 +72,6 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
 
 
 
