899327124ba0536d9b5b7079b4ab7d693fb3392c
Tavis Weeden <tavisweeden@gmail.com>
2019-11-03 03:42:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |season, holiday|
     if holiday.include?("BBQ")
-      puts season.to_s
+      puts holiday.to_s
       
 end
 end
